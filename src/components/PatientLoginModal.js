dc119caf22d41b3837569d118a8f4bcba8175cbe
John Pham <phambinouh@gmail.com>
2024-03-16 16:24:15 -0500

    Cleaned up bloat css and changed input placeholders

--- src/components/PatientLoginModal.js
+++ src/components/PatientLoginModal.js
@@ -7,7 +7,7 @@ function PatientLoginModal() {
       <div class="login-page">
         <div class="form">
           <form class="login-form">
-            <input type="text" placeholder="username" />
+            <input type="text" placeholder="patientid" />
             <input type="password" placeholder="password" />
             <button>login</button>
             <p class="message">New patient? <a href="/patient/register">Register here!</a></p>
