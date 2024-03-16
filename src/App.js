e43c70c892db49fb019347d5d6c78b9338c38a13
emar <egmarinn@cougarnet.uh.edu>
2024-03-15 22:02:25 -0500

    add path for sign up page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
 
 import './App.css';
 import PatientLogIn from './pages/patient/LogIn';
+import PatientSignUp from './pages/patient/SignUp';
 import PatientHome from './pages/patient/PatientHome';
 import EmployeeHome from './pages/employee/EmployeeHome';
 import EmployeeLogIn from './pages/employee/EmployeeLogIn';
@@ -17,6 +18,7 @@ function App() {
         <Route path='/' element={<PatientHome />} />
         <Route path='/patient' element={<PatientHome />} />
         <Route path='/patient/login' element={<PatientLogIn />} />
+        <Route path="/patient/signup" element={<PatientSignUp />} />
 
         <Route path='/employee' element={<EmployeeHome />} />
         <Route path='/employee/login' element={<EmployeeLogIn />} />
--- src/pages/patient/LogIn.js
+++ src/pages/patient/LogIn.js
@@ -1,3 +1,4 @@
+import { NavLink as Link } from 'react-router-dom';
 import PatientNavbar from '../../components/PatientNavbar';
 
 function PatientLogIn() {
@@ -6,6 +7,8 @@ function PatientLogIn() {
       <PatientNavbar />
 
       <h1>Patient log in page</h1>
+
+      <p>Dont have an account ?<Link to="/patient/signup">Sign Up</Link></p>
     </>
   );
 }
