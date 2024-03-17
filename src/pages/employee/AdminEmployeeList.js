920923dbd02df00c77a21654a844b6a689b58ec8
yeseniardz <yeseniarodr314@gmail.com>
2024-03-17 10:34:37 -0500

    Create page and route for Employee List

---
+++ src/pages/employee/AdminEmployeeList.js
@@ -0,0 +1,12 @@
+import Navbar from "../../components/Navbar";
+
+function DisplayEmployees(){
+    return(
+        <>
+        <Navbar />
+        <h1>Employee List goes here</h1>
+        </>
+    );
+}
+
+export default DisplayEmployees;
