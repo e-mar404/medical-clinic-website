e45323ee5626a6365728d46f2e7bf5f472c800e5
yeseniardz <yeseniarodr314@gmail.com>
2024-03-19 15:30:38 -0500

    Add New Admin Pages

--- src/pages/employee/AdminEmployeeList.js
+++ src/pages/employee/AdminEmployeeList.js
@@ -1,10 +1,21 @@
 import Navbar from "../../components/Navbar";
+import DisplayEmployee from "../../components/DisplayEmployee";
+import AddNewEmployee from "../../components/AddNewEmployee";
+import SearchForEmployee from "../../components/SearchForEmployee";
 
 function DisplayEmployees(){
     return(
         <>
-        <Navbar />
-        <h1>Employee List goes here</h1>
+            <Navbar />
+            <div style={{ display: 'flex', alignItems: 'center', padding:'0 0 15px', margin:'0 0 0', justifyContent: "space-between"}}>
+                    <div><h1>Employee List</h1></div>
+                <div>
+                    <SearchForEmployee />
+                    <AddNewEmployee />
+                </div>
+            </div>
+
+            <DisplayEmployee />
         </>
     );
 }
