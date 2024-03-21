ea4f3769b754f42439080cd4561c77a2c30a4dc1
emar <egmarinn@cougarnet.uh.edu>
2024-03-21 16:38:17 -0500

    removed comments

--- src/App.js
+++ src/App.js
@@ -17,15 +17,12 @@ import ViewDoctorAppointments from './pages/employee/ViewDoctorAppointments';
 import TransferDoctor from './pages/employee/TransferDoctor';
 
 function App() {
-  // On first-time load
   if (localStorage.getItem("Loaded") === null) {
     localStorage.setItem("Loaded", true)
     localStorage.setItem("LoggedIn", false)
     localStorage.setItem("LoginEmail", null) // Email of logged in user
     localStorage.setItem("LoginType", null) // Type of logged in user (patient, employee)
   }
-
-  // localStorage.clear();
 
   return (
     <Router>
