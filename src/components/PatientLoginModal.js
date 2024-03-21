ea2bf023e28d7b6b9a057e3f26c8035bc4551ee7
John Pham <phambinouh@gmail.com>
2024-03-20 22:08:17 -0500

    Added back-end logic to the patient login

--- src/components/PatientLoginModal.js
+++ src/components/PatientLoginModal.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LoginModal.css';
 
 function PatientLoginModal() {
-  
-  const loginFunction = () => {
-    localStorage.setItem("LoggedIn", true);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState(''); 
+
+  const loginFunction = (e) => {
+    e.preventDefault();
+
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        'email': email,
+        'password': password,
+      })
+    };
+
+    fetch('/patient/login', requestOptions).then((response) => {
+      response.json().then((data) => {
+        console.log(response.status);
+      });
+    });
+
+    //localStorage.setItem("LoggedIn", true);
+    //console.log(email, password);
   }
 
   return (
@@ -12,8 +32,8 @@ function PatientLoginModal() {
       <div className="login-page">
         <div className="form">
           <form className="login-form">
-            <input type="text" placeholder="patientid" />
-            <input type="password" placeholder="password" />
+            <input type="text" placeholder="patientid" onChange={(e) => setEmail(e.target.value)} />
+            <input type="password" placeholder="password" onChange={(e) => setPassword(e.target.value)} />
             <button onClick={loginFunction}>login</button>
             <p className="message">New patient? <a href="/patient/register">Register here!</a></p>
           </form>
