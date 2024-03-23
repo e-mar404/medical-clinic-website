464a169a6a13c0bcd4c067f0fe0d9c5e7723cb79
John Pham <phambinouh@gmail.com>
2024-03-23 14:46:29 -0500

    Navbar stuff

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,133 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom'; 
+import { useNavigate } from 'react-router-dom';
 import './Navbar.css';
 
+function Username() {
+  return (
+    <a className="nav-link dropdown-toggle btn btn-lg" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+      <span id="navFirstName">{localStorage.getItem("UserFirstName")}</span> <span id="navLastName">{localStorage.getItem("UserLastName")}</span>
+    </a>
+  )
+}
+
+function SignOutButton() {
+  let nav = useNavigate();
+  return (
+    <li className="nav-item">
+      <a className="nav-link btn btn-lg" aria-current="page" href="/" onClick={function () {
+        localStorage.clear();
+        nav('/', {});
+      }}>Sign Out</a>
+    </li>
+  )
+}
+
 function Navbar() {
-  const nav = useNavigate();
-  const RenderButtons = () => {
-    if (localStorage.getItem("LoggedIn") === "true") {
+  const RenderLeftSide = () => {
+    if (localStorage.getItem("UserType") === 'Patient') {
       return (
-        <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <a className="nav-link" aria-current="page" href="/">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link disabled" href="/">Appointments</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link disabled" href="/">Locations</a>
+          </li>
           <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle btn btn-lg" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              <span id="navFirstName">{localStorage.getItem("UserFirstName")}</span> <span id="navLastName">{localStorage.getItem("UserLastName")}</span>
+            <a className="nav-link dropdown-toggle disabled" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Services
             </a>
             <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a className="dropdown-item" href="/employee/login">Profile Information</a></li>
-            <li><a className="dropdown-item" href="/employee/login">Medical History</a></li>
-            <div className="dropdown-divider"></div>
-            <li><a className="dropdown-item" href="/employee/login">Schedule Appointment</a></li>
+              <li><a className="dropdown-item" href="/">Cancer</a></li>
+              <li><a className="dropdown-item" href="/">Children's Services</a></li>
+              <li><a className="dropdown-item" href="/">Heart & Vascular</a></li>
+              <li><a className="dropdown-item" href="/">Neuroscience</a></li>
+              <li><a className="dropdown-item" href="/">Surgical Services</a></li>
+              <li><a className="dropdown-item" href="/">Transplant</a></li>
             </ul>
           </li>
-
+        </ul>
+      )
+    }
+    else if (localStorage.getItem("UserType") === 'Doctor' || localStorage.getItem("UserType") === 'Nurse') {
+      return (
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <a className="nav-link btn btn-lg" aria-current="page" href="/" onClick={SignOut}>Sign Out</a>
+            <a className="nav-link" aria-current="page" href="/employee">Employee Home</a>
+          </li>
+        </ul>
+      )
+    }
+    else if (localStorage.getItem("UserType") === 'Administrator') {
+      return (
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <a className="nav-link" aria-current="page" href="/admin">Admin Home</a>
           </li>
         </ul>
       )
     }
     else {
+      return (
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <a className="nav-link" aria-current="page" href="/">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link disabled" href="/">Appointments</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link disabled" href="/">Locations</a>
+          </li>
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle disabled" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Services
+            </a>
+            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a className="dropdown-item" href="/">Cancer</a></li>
+              <li><a className="dropdown-item" href="/">Children's Services</a></li>
+              <li><a className="dropdown-item" href="/">Heart & Vascular</a></li>
+              <li><a className="dropdown-item" href="/">Neuroscience</a></li>
+              <li><a className="dropdown-item" href="/">Surgical Services</a></li>
+              <li><a className="dropdown-item" href="/">Transplant</a></li>
+            </ul>
+          </li>
+        </ul>
+      )
+
+    }
+  }
+  const RenderRightSide = () => {
+    if (localStorage.getItem("LoggedIn") === "true" && localStorage.getItem("UserType") === 'Patient') {
+      return (
+        <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
+          <li className="nav-item dropdown">
+            {Username()}
+            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a className="dropdown-item" href="/employee/login">Profile Information</a></li>
+              <li><a className="dropdown-item" href="/employee/login">Medical History</a></li>
+              <div className="dropdown-divider"></div>
+              <li><a className="dropdown-item" href="/employee/login">Schedule Appointment</a></li>
+            </ul>
+          </li>
+          {SignOutButton()}
+        </ul>
+      )
+    }
+    else if (localStorage.getItem("LoggedIn") === "true" && localStorage.getItem("UserType") === 'Doctor') {
+      return (
+        <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
+          <li className="nav-item dropdown">
+            {Username()}
+          </li>
+          {SignOutButton()}
+        </ul>
+      )
+    }
+    else { // Not signed in (default)
       return (
         <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
           <li className="nav-item dropdown">
@@ -50,11 +151,6 @@ function Navbar() {
       )
     }
   }
-
-  const SignOut = () => {
-    localStorage.clear();
-    nav('/', {});
-  }
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-info">
@@ -64,31 +160,8 @@ function Navbar() {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link" aria-current="page" href="/">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">Appointments</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">Locations</a>
-              </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Services
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><a className="dropdown-item" href="/">Cancer</a></li>
-                  <li><a className="dropdown-item" href="/">Children's Services</a></li>
-                  <li><a className="dropdown-item" href="/">Heart & Vascular</a></li>
-                  <li><a className="dropdown-item" href="/">Neuroscience</a></li>
-                  <li><a className="dropdown-item" href="/">Surgical Services</a></li>
-                  <li><a className="dropdown-item" href="/">Transplant</a></li>
-                </ul>
-              </li>
-            </ul>
-            {RenderButtons()}
+            {RenderLeftSide()}
+            {RenderRightSide()}
           </div>
         </div>
       </nav>
