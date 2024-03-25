cb5b70ce3376b0139a1372903bd7b7156b9e106f
emar <egmarinn@cougarnet.uh.edu>
2024-03-25 13:49:58 -0500

    fix: make appointment only gets doctors

--- server/server.js
+++ server/server.js
@@ -93,10 +93,14 @@ const server = http.createServer((req, res) => {
           getEmployeesByType(res, db, type);
           break;
 
-        case /\/employee\/byclinic/.test(req.url):
-          const clinic_id = req.url.split('/')[3];
+        case /\/employee\/byclinic\//.test(req.url):
+          const args = req.url.split('/');
 
-          getEmployeesByClinic(res, db, clinic_id);
+          const clinic_id = args[3];
+          const role = (args.length > 3) ? args[4] : '';
+          console.log(`Calling get employees with role: ${role}`);
+            
+          getEmployeesByClinic(res, db, clinic_id, role);
           break;
 
         case /get_clinics/.test(req.url):
