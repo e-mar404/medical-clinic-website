4bb4b35c50f1d1403fb207e68cfaa9714338fb73
emar <egmarinn@cougarnet.uh.edu>
2024-04-01 18:48:14 -0500

    fetch /create_referral on front end + err handling

--- server/controllers/referralController.js
+++ server/controllers/referralController.js
@@ -5,20 +5,22 @@ async function createReferral(req, res, db) {
     const body = await PostData(req);
     const { doctor_id, patient_id, expiration_date } = JSON.parse(body);
 
-    db.query('INSERT INTO Referral(patient_id, doctor_id, expiration_date) VALUES(?, ?, DATE ?)', [doctor_id, patient_id, expiration_date], (err, _db_res) => {
-      if (err) {
-        throw err;
-      }
-      
-      const msg = 'Referral created successfully';
-      console.log(msg);
-      
-      res.writeHead(200, headers);
-      res.end(JSON.stringify ({ message: msg }));
+    const msg = await new Promise((resolve, reject) => {
+      db.query('INSERT INTO Referral(patient_id, doctor_id, expiration_date) VALUES(?, ?, DATE ?)', [doctor_id, patient_id, expiration_date], (err, _db_res) => {
+        if (err) {
+          reject(err.sqlMessage);
+        }
+
+        const msg = 'Referral created successfully';
+        console.log(msg);
+        
+        resolve(msg);
+      });
     });
 
-  }
-  catch (err) {
+    res.writeHead(200, headers);
+    res.end(JSON.stringify ({ message: msg }));
+  } catch (err) {
     res.writeHead(400, headers);
     res.end(JSON.stringify ({ error: err }));
   }
--- src/components/ReferralsForm.js
+++ src/components/ReferralsForm.js
@@ -1,4 +1,5 @@
 import { useEffect, useState, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 import DatePicker from 'react-datepicker'; 
 
 function ReferralsForm() {
@@ -53,36 +54,105 @@ function ReferralsForm() {
     console.log('use effect called');
   }, [specialistsRef, patientsRef]); 
 
+  const [formData, setFormData] = useState({
+    "doctor_id": -1,
+    "patient_id": -1,
+    "expiration_date": null 
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    console.log(name, value);
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const nav = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    if (formData.doctor_id === -1) {
+      alert('Plese select a specialist');
+      return;
+    }
+
+    if (formData.patient_id === -1) {
+      alert('Please select a patient');
+      return;
+    }
+    
+    const referralData = {
+      doctor_id: formData.doctor_id,
+      patient_id: formData.patient_id,
+      expiration_date: formData.expiration_date.toISOString().slice(0, 10)
+    };
+
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(referralData)
+    };
+
+    fetch(`${process.env.REACT_APP_BACKEND_HOST}/create_referral`, requestOptions).then((response) => {
+      response.json().then((data) => {
+        console.log(data);
+
+        if (response.status !== 200) {
+          alert(data.error);
+          return;
+        }
+
+        alert(data.message);
+
+        nav('/doctor', {});
+      });
+    });
+  };
+
   return(
     <div className="login-page">
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
 
         <label className="d-flex justify-content-center text-secondary">Specialist:</label>    
         <select
-          name="specialistId"
+          name="doctor_id"
+          value={formData.doctor_id}
+          onChange={handleInputChange}
           defaultValue={-1}
           required
           >
           <option key={0} value={-1} disabled>Select a specialist</option>
-          {specialists.map((specialist) => (
+          {specialists.map(specialist => (
             <option key={specialist.employee_id} value={specialist.employee_id}>Dr. {specialist.first_name} {specialist.last_name}</option>
           ))}
         </select>
 
         <label className="d-flex justify-content-center text-secondary">Patient:</label>
         <select 
-          name="patientId" 
+          name="patient_id"
+          value={formData.patient_id}
+          onChange={handleInputChange}
           defaultValue={-1}
           required >
           <option key={0} value={-1} disabled>Select one of your patients</option>
-          {patients.map((patient) => (
+          {patients.map(patient => (
             <option key={patient.patient_id} value={patient.patient_id}>{patient.patient_fname} {patient.patient_lname}</option>
           ))}
         </select>
 
         <label className="d-flex text-secondary">Referral valid through:</label>
-        <DatePicker name="date" dateFormat="yyyy-MM-dd" showIcon toggleCalendarOnIconClick required />
-            
+        <DatePicker 
+          name="date"
+          selected={formData.expiration_date}
+          onChange={expiration_date => setFormData({ ...formData, expiration_date })}
+          dateFormat="yyyy-MM-dd"
+          showIcon
+          toggleCalendarOnIconClick
+          required />     
+
         <button className="submit-button" type="submit">Refer Patient</button>
       </form>
     </div>
