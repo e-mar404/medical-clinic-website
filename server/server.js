fb5190e2aa116501ed02d5adf8df2b378540ee31
MohammadBakeer <159083259+MohammadBakeer@users.noreply.github.com>
2024-04-16 16:48:51 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const http = require('http');
 const mysql = require('mysql2');
-const { generateReportFor } = require('./controllers/reportController');
-const { createAppointment, getClinicAppointments } = require('./controllers/appointmentController');
+
+const { generateReportFor, getNewUsersReport, generateDoctorTotal } = require('./controllers/reportController');
+const { createAppointment, getClinicAppointments, availableAppointments, getClinicOfReceptionist, updateAppointmentStatus } = require('./controllers/appointmentController');
 const { getClinics } = require('./controllers/clinicController');
 const { headers } = require('./utils');
-const { createPatientAccount, loginPatient } = require('./controllers/patientController');
+const { createPatientAccount, loginPatient, getPatientProfile, postPatientProfile, getPatientMedicalHistory, updatePatientMedicalHistory } = require('./controllers/patientController');
+const { prescribeMedicationToPatient, getMedicationsForPatient, removeMedicationForPatient } = require('./controllers/medicationsController')
+const { patientCharges } = require('./controllers/billingController');
+
 const { createReferral } = require('./controllers/referralController');
 const {
   getEmployeesByType,
@@ -13,7 +17,12 @@ const {
   createEmployeeAccount,
   employeeTransfer,
   getSpecialists,
-  getPatientsOf
+  getPatientsOf,
+  getAppointments,
+  getDoctorInformation,
+  getAdminClinic,
+  getClinicEmployees
+
 } = require('./controllers/employeeController');
 
 require('dotenv').config();
@@ -24,129 +33,231 @@ const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
 const database = process.env.DATABASE;
 
-const db = mysql.createConnection({
+
+const pool = mysql.createPool({
   host: dbHost,
   port: dbPort,
   user: dbUser,
   password: dbPassword,
-  database: database
+  database: database,
+
+  enableKeepAlive: true
 });
 
-db.connect(function (err) {
-  const msg = (err) ? `Server.js: Error connecting to db: ${err}` : `Server.js: Database '${database}' connected`;  
-
-  console.log(msg);
-});
 
 const server = http.createServer((req, res) => {
-    
-  console.log(`Server.js: METHOD: ${req.method}; URL: ${req.url}`);
+  pool.getConnection(function (err, db) {
+    const msg = (err) ? `Server.js: Error connecting to db: ${err}` : `Server.js: Database '${database}' connected`;  
 
-  switch (req.method) {
-    case 'OPTIONS':
-      res.writeHead(200, headers); 
-      res.end();
-      break;
+    console.log(msg);
 
-    case 'POST':
-      switch (req.url) {
-        case '/patient/register':
-          createPatientAccount(req, res, db);
-          break;
+    let patient_id;
+    let clinic_id
+    console.log(`Server.js: METHOD: ${req.method}; URL: ${req.url}`);
 
-        case '/patient/login':
-          loginPatient(req, res, db);
-          break;
+    switch (req.method) {
+      case 'OPTIONS':
+        res.writeHead(200, headers); 
+        res.end();
+        break;
 
-        case '/employee/login':
-          loginEmployee(req, res, db);
-          break;
+      case 'POST':
+        switch (req.url) {
+          case '/patient/register':
+            createPatientAccount(req, res, db);
+            break;
 
-        case '/create_referral':
-          createReferral(req, res, db);
-          break;
+          case '/patient/login':
+            loginPatient(req, res, db);
+            break;
 
-        case '/make_appointment': 
-          createAppointment(req, res, db);
-          break;
+          case '/patient/profile':
+            postPatientProfile(req, res, db);
+            break;
 
-        case '/admin/newemployee':
-          createEmployeeAccount(req, res, db);
-          break; 
+          case '/employee/login':
+            loginEmployee(req, res, db);
+            break;
+
+          case '/update_patient_medical_history':
+            updatePatientMedicalHistory(req, res, db);
+            break;
+
+          case '/prescribe_medication':
+            prescribeMedicationToPatient(req, res, db);
+            break;
+
+          case '/remove_medication':
+            removeMedicationForPatient(req, res, db);
+            break;
+
+          case '/create_referral':
+            createReferral(req, res, db);
+            break;
+
+          case '/make_appointment': 
+            createAppointment(req, res, db);
+            break;
+
+            case '/appointmentStatus':
+              updateAppointmentStatus(req, res, db);
+              break;
+
+          case '/available_appointments':
+            availableAppointments(req, res, db);
+            break;
+
+          case '/admin/newemployee':
+            createEmployeeAccount(req, res, db);
+            break; 
+
+          case '/admin/transfer':
+            employeeTransfer(req, res, db);
+            break;
+
+          default:
+            res.writeHead(404, headers);
+            res.end(JSON.stringify({ message: 'Route not found' }));
+            break;
+        }
+
+        break;
+
+      case 'GET': 
+        switch (true){
+          case /patientBilling/.test(req.url):
+            
+            patientCharges(req, res, db);
+            break;
+
+          case /reports/.test(req.url): 
+            const reportType = req.url.split('/')[2];
+
+            generateReportFor(res, db, reportType);
+            break;
+
+          case /\/patient\/profile/.test(req.url):
+            patient_id = req.url.split('/')[3];
+            getPatientProfile(res, db, patient_id);
+            break;
+
+          case /\/history_for_patient/.test(req.url):
+            patient_id = req.url.split('/')[2];
+
+            getPatientMedicalHistory(res, db, patient_id);
+            break;
+
+          case /\/medications_for_patient/.test(req.url):
+            patient_id = req.url.split('/')[2];
+
+            getMedicationsForPatient(res, db, patient_id);
+            break;
+
+          case /\/employee\/bytype/.test(req.url): 
+            const type = req.url.split('/')[3];
+
+            getEmployeesByType(res, db, type);
+            break;
+
+          case /\/employee\/specialists/.test(req.url):
+            getSpecialists(res, db);
+            break;
+
+          case /\/employee\/patients_of/.test(req.url):
+            const doctor_id = req.url.split('/')[3];
+
+            getPatientsOf(res, db, doctor_id);
+            break;
+
+            case /\/employee\/byclinic\//.test(req.url):
+            const args = req.url.split('/');
+
+            const clin_id = args[3];
+            const role = (args.length > 3) ? args[4] : '';
+            console.log(`Calling get employees with role: ${role}`);
+
+            getEmployeesByClinic(res, db, clin_id, role);
+            break;
           
-        case '/admin/transfer':
-          employeeTransfer(req, res, db);
-          break;
+          case /clinicAppointments/.test(req.url):
+            getClinicAppointments(res, db, req);
+            break;
 
-        default:
+          case /get_clinics/.test(req.url):
+            getClinics(res, db);
+            break; 
+
+
+          case /\/admin\/employeelist/.test(req.url): //might not be needed
+            getEmployeesByType(res, req, type);
+            break;
+
+          case /viewappointment/.test(req.url): // should get employee id and then call function that returns all appointments
+            const empId = req.url.split('/')[2];
+            getAppointments(res, db, empId);
+            //res.writeHead(404, headers);
+            //res.end(JSON.stringify({ message: 'Route for appointment' }));
+            break;
+
+          case /get_doctor/.test(req.url):
+            const doctorID = req.url.split('/')[2];
+            getDoctorInformation(res, db, doctorID);
+            //res.writeHead(404, headers);
+            //res.end(JSON.stringify({ message: 'Route for doctor info' }));
+            break;
+
+          case /accounts_created/.test(req.url):
+            const start_date = req.url.split('/')[2];
+            const end_date =  req.url.split('/')[3];
+            const clinic_id = req.url.split('/')[4];
+            getNewUsersReport(res, db, start_date, end_date, clinic_id);
+            break;
+
+        case /doctor_total/.test(req.url):
+            const startDate = req.url.split('/')[2];
+            const endDate =  req.url.split('/')[3];
+            const clinicId = req.url.split('/')[4];
+            generateDoctorTotal(res, db, startDate, endDate, clinicId);
+            break;
+
+        case /get_report/.test(req.url):
           res.writeHead(404, headers);
-          res.end(JSON.stringify({ message: 'Route not found' }));
-          break;
-      }
-
-      break;
-
-    case 'GET': 
-      switch (true){
-        case /reports/.test(req.url): 
-          const reportType = req.url.split('/')[2];
-
-          generateReportFor(res, db, reportType);
-          break;
-
-        case /\/employee\/bytype/.test(req.url): 
-          const type = req.url.split('/')[3];
-
-          getEmployeesByType(res, db, type);
+          res.end(JSON.stringify({ message: 'Route for reports' }));
           break;
         
-        case /\/employee\/specialists/.test(req.url):
-          
-          getSpecialists(res, db);
+        case /getAdminClinic/.test(req.url):
+          const adminEmail = req.url.split('/')[2];
+          getAdminClinic(res, db, adminEmail);
           break;
 
-        case /\/employee\/patients_of/.test(req.url):
-          const doctor_id = req.url.split('/')[3];
-
-          getPatientsOf(res, db, doctor_id);
-          break;
-
-        case /\/employee\/byclinic\//.test(req.url):
-          const args = req.url.split('/');
-
-          const clinic_id = args[3];
-          const role = (args.length > 3) ? args[4] : '';
-          console.log(`Calling get employees with role: ${role}`);
-            
-          getEmployeesByClinic(res, db, clinic_id, role);
-          break;
-
-        case /clinicAppointments/.test(req.url):
-          getClinicAppointments(req, res, db);
-          break;
-          
-        case /get_clinics/.test(req.url):
-          getClinics(res, db);
-          break; 
-
+        case /\/getClinicOfReceptionist\/\d+/.test(req.url):
+          const userId = req.url.split('/')[2];
       
-        case /\/admin\/employeelist/.test(req.url): //might not be needed
-          getEmployeesByType(res, req, type);
+          getClinicOfReceptionist(res, db, userId);
+          break;      
+
+        
+        case /getClinicEmployees/.test(req.url):
+          const adminClinic = req.url.split('/')[2];
+          getClinicEmployees(res, db, adminClinic);
           break;
+
 
         default:
-          res.writeHead(404, headers);
-          res.end(JSON.stringify({ message: 'Route not found' }));
-          break;
-      }
+            res.writeHead(404, headers);
+            res.end(JSON.stringify({ message: 'Route not found' }));
+            break;
+        }
 
-      break;
+        break;
 
-    default:
-      res.writeHead(500, headers);
-      res.end(JSON.stringify({ message: 'Invalid request type' }));
-      break;
-  }
+      default:
+        res.writeHead(500, headers);
+        res.end(JSON.stringify({ message: 'Invalid request type' }));
+        break;
+    }
+    pool.releaseConnection(db);
+  });
 });
 
 const PORT = process.env.PORT || 5001; 
