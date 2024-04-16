f02efa8edccc40340302d69f0f1a95bee5669ec3
MohammadBakeer <159083259+MohammadBakeer@users.noreply.github.com>
2024-04-16 16:52:03 -0500

    Update MedicalHistoryForm.js

--- src/components/MedicalHistoryForm.js
+++ src/components/MedicalHistoryForm.js
@@ -1,135 +1,207 @@
-import { useState, useEffect, useRef } from 'react';
-<<<<<<< HEAD
-import PrescribeMedicationForm from './PrescribeMedicationForm.js';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Navbar.css';
 
-function MedicalHistoryForm({ patient_id }) {
-=======
-import { useParams } from 'react-router-dom';
-import PrescribeMedicationForm from './PrescribeMedicationForm.js';
+function SignOutButton() {
+  let nav = useNavigate();
+  return (
+    <li className="nav-item">
+      <a className="nav-link btn btn-lg" aria-current="page" href="/" onClick={function () {
+        localStorage.clear();
+        nav('/', {});
+      }}>Sign Out</a>
+    </li>
+  )
+}
 
-function MedicalHistoryForm() {
-  const { patient_id } = useParams();
-  console.log(patient_id);
+function Navbar() {
+  const RenderLeftSide = () => {
+    if (localStorage.getItem("UserType") === 'Patient') {
+      return (
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <a className="nav-link" aria-current="page" href="/patient">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link disabled" href="/">Appointments</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link disabled" href="/">Locations</a>
+          </li>
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle disabled" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Services
+            </a>
+            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a className="dropdown-item" href="/">Cancer</a></li>
+              <li><a className="dropdown-item" href="/">Children's Services</a></li>
+              <li><a className="dropdown-item" href="/">Heart & Vascular</a></li>
+              <li><a className="dropdown-item" href="/">Neuroscience</a></li>
+              <li><a className="dropdown-item" href="/">Surgical Services</a></li>
+              <li><a className="dropdown-item" href="/">Transplant</a></li>
+            </ul>
+          </li>
+        </ul>
+      )
+    }
+    else if (localStorage.getItem("UserType") === 'Doctor' || localStorage.getItem("UserType") === 'Nurse') {
+      return (
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <a className="nav-link" aria-current="page" href="/doctor">Doctor Home</a>
+          </li>
+        </ul>
+      )
+    }
+    else if (localStorage.getItem("UserType") === 'Administrator') {
+      return (
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <a className="nav-link" aria-current="page" href="/admin">Admin Home</a>
+          </li>
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Employees
+            </a>
+            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a className="dropdown-item" href="/admin/employeelist">View Employees</a></li>
+              <div class="dropdown-divider"></div>
+              <li><a className="dropdown-item" href="/admin/newemployee">Add an Employee</a></li>
+            </ul>
+          </li>
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Appointments
+            </a>
+            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a className="dropdown-item" href="/admin/viewappointment">View Appointments</a></li>
+              <div class="dropdown-divider"></div>
+              <li><a className="dropdown-item" href="/admin/make_appointment">Create Appointment</a></li>
+            </ul>
+          </li>
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Clinics
+            </a>
+            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a className="dropdown-item" href="/admin/transfer">Transfer a Doctor</a></li>
+            </ul>
+          </li>
+        </ul>
+      )
+    }
+    else if (localStorage.getItem("UserType") === 'Receptionist') {
+        // Receptionist navigation links
+        return (
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link" aria-current="page" href="/receptionist">Home</a>
+            </li>
+            {/* Add other receptionist-specific navigation links here */}
+          </ul>
+        )
+    }
+    else {
+      return (
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <a className="nav-link" aria-current="page" href="/">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link disabled" href="/">Appointments</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link disabled" href="/">Locations</a>
+          </li>
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle disabled" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Services
+            </a>
+            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a className="dropdown-item" href="/">Cancer</a></li>
+              <li><a className="dropdown-item" href="/">Children's Services</a></li>
+              <li><a className="dropdown-item" href="/">Heart & Vascular</a></li>
+              <li><a className="dropdown-item" href="/">Neuroscience</a></li>
+              <li><a className="dropdown-item" href="/">Surgical Services</a></li>
+              <li><a className="dropdown-item" href="/">Transplant</a></li>
+            </ul>
+          </li>
+        </ul>
+      )
 
->>>>>>> 17a7d0deb42d02923f62764929a60344d2cddfd7
-  const [patientMedicalHistory, setPatientMedicalHistory] = useState({'conditions': '', 'allergies': '', 'family_history': '', 'patient_id': patient_id });
-  const patientMedicalHistoryRef = useRef();
-  
-  useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json' }
-    };
-
-    fetch(`${process.env.REACT_APP_BACKEND_HOST}/history_for_patient/${patient_id}`, requestOptions).then((response) => {
-      response.json().then((data) => {
-        if (response.status !== 200) {
-          alert(data.error);
-          return;
-        }
-
-        setPatientMedicalHistory({
-          ...data.message,
-<<<<<<< HEAD
-          'patient_id':patient_id
-=======
-          'patient_id': patient_id
->>>>>>> 17a7d0deb42d02923f62764929a60344d2cddfd7
-        });
-      });
-    });
-
-  }, [patientMedicalHistoryRef, patient_id]);
-
-  const updatePatientMedicalHistory = () => {
-    const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(patientMedicalHistory)
-    };
-
-    fetch(`${process.env.REACT_APP_BACKEND_HOST}/update_patient_medical_history`, requestOptions).then((response) => {
-      response.json().then((data) => {
-        console.log(data);
-
-        if (response.status !== 200) {
-          alert(data.error);
-          return;
-        }
-
-        alert(data.message);
-      });
-    });
+    }
   }
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-
-    setPatientMedicalHistory({
-      ...patientMedicalHistory,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    console.log(patientMedicalHistory);
-
-    updatePatientMedicalHistory();
-  };
-
+  const RenderRightSide = () => {
+    if (localStorage.getItem("LoggedIn") === "true" && localStorage.getItem("UserType") === 'Patient') {
+      return (
+        <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle btn btn-lg" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <span id="navFirstName">{localStorage.getItem("UserFirstName")}</span> <span id="navLastName">{localStorage.getItem("UserLastName")}</span><span> ({localStorage.getItem("UserType")})</span>
+            </a>
+            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a className="dropdown-item" href="/patient/profile">Profile Information</a></li>
+              <li><a className="dropdown-item disabled" href="/">Medical History</a></li>
+              <div className="dropdown-divider"></div>
+              <li><a className="dropdown-item" href="/make_appointment">Schedule Appointment</a></li>
+            </ul>
+          </li>
+          {SignOutButton()}
+        </ul>
+      )
+    }
+    else if (localStorage.getItem("LoggedIn") === "true" && ((localStorage.getItem("UserType") === 'Doctor' || localStorage.getItem("UserType") === 'Nurse') || localStorage.getItem("UserType") === 'Receptionist' || localStorage.getItem("UserType") === 'Administrator')) {
+      return (
+        <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
+          <li className="nav-item dropdown">
+          <a className="btn btn-lg disabled" href="/" role="button">
+            <span id="navFirstName">{localStorage.getItem("UserFirstName")}</span> <span id="navLastName">{localStorage.getItem("UserLastName")}</span><span> ({localStorage.getItem("UserType")})</span>
+          </a>
+          </li>
+          {SignOutButton()}
+        </ul>
+      )
+    }
+    else { // Not signed in (default)
+      return (
+        <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle btn btn-lg" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Employees
+            </a>
+            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a className="dropdown-item" href="/employee/login">Employee Login</a></li>
+            </ul>
+          </li>
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle btn btn-lg" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Patients
+            </a>
+            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a className="dropdown-item" href="/patient/login">Patient Login</a></li>
+              <li><a className="dropdown-item" href="/patient/signup">Patient Register</a></li>
+            </ul>
+          </li>
+        </ul>
+      )
+    }
+  }
   return (
     <>
-      <form id="ProfileForm" className="form-control m-4 w-auto" onSubmit={handleSubmit}>
-        <span className="h4 m-0">Patient History for {patient_id}</span>
-
-        <div className="col">
-          <div className="mr ml">
-            <label>Name</label>
-            <input type="text" className="form-control" id="FullName" placeholder="Patient Name" readOnly />
+      <nav className="navbar navbar-expand-lg navbar-light bg-info">
+        <div id="navbarContainer" className="container-fluid">
+          <a className="navbar-brand" href="/">Medical Clinic Application</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            {RenderLeftSide()}
+            {RenderRightSide()}
           </div>
         </div>
-
-        <div className="col">
-          <div className="mr ml">
-            <label>Conditions</label>
-            <input type="text" className="form-control" id="MedicalConditions" name="conditions" placeholder="Medical Conditions" defaultValue={patientMedicalHistory.conditions} onChange={handleInputChange}/>
-          </div>
-        </div>
-
-        <div className="col">
-          <div className="mr ml">
-            <label>Allergies</label>
-            <input type="text" className="form-control" id="Allergies" defaultValue={patientMedicalHistory.allergies} />
-          </div>
-        </div>
-
-        <div className="col">
-          <div className="mr ml">
-            <label>Family History</label>
-            <input type="text" className="form-control" id="FamilyHistory" defaultValue={patientMedicalHistory.family_history}/>
-          </div>
-        </div>
-<<<<<<< HEAD
-
-=======
->>>>>>> 17a7d0deb42d02923f62764929a60344d2cddfd7
-
-        <div className="row">
-          <div className="col">
-            <button type="submit" className="btn btn-primary btn float-end" id="SaveProfile">Save History</button>
-          </div>
-        </div>
-      </form>
-      
-<<<<<<< HEAD
-      <PrescribeMedicationForm />
-=======
-      <PrescribeMedicationForm patient_id={patient_id} doctor_id={localStorage.getItem('UserId')} />
->>>>>>> 17a7d0deb42d02923f62764929a60344d2cddfd7
+      </nav>
     </>
   );
 }
 
-export default MedicalHistoryForm;
+export default Navbar;
